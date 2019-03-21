6c43d18153c63702dedbdf3b99ec31ed5f938c46
javierordz <javier.ordz@gmail.com>
2019-03-21 11:17:56 -0500

    jumbotron, selection choices

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyDepgR1SijxhD2GyI_ib3geDWBk2RPNGpE",
+    authDomain: "rpsmultiplayer-73adb.firebaseapp.com",
+    databaseURL: "https://rpsmultiplayer-73adb.firebaseio.com",
+    projectId: "rpsmultiplayer-73adb",
+    storageBucket: "",
+    messagingSenderId: "827356996393"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
