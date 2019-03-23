2a11cb0d7e0b5e4c2c319e6e03cefe0b85cc2030
javierordz <javier.ordz@gmail.com>
2019-03-22 23:59:05 -0500

    javascript added, readme.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RPS-Multiplayer
 Unit 7 Assignment
+
+(1) I made this using Firebase's Firestore, which while more flexible, was exceedingly more difficult
+than the Realtime databse. In particular, the lack of "onDisconnect()" made restricting only players very hard.
+
+If I'd had more time, I'd probably have tried to re-engineering everything into the Realtime or 
+used an "online" Boolean to monitor connection status tied with a Cloud Function to detect changes
+to "online" and delete the player document from "Players" on Firestore.
+Right now, you have to manually delete the players for others to join.
+
+(2) The chat functionality really would have benefitted from Realtime, as well. I could make it work
+with Firestore, but it doesn't seem as smooth a solution.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,104 @@
-var config = {
-    apiKey: "AIzaSyDepgR1SijxhD2GyI_ib3geDWBk2RPNGpE",
-    authDomain: "rpsmultiplayer-73adb.firebaseapp.com",
-    databaseURL: "https://rpsmultiplayer-73adb.firebaseio.com",
-    projectId: "rpsmultiplayer-73adb",
-    storageBucket: "",
-    messagingSenderId: "827356996393"
-};
+// VARIABLES
+var db = firebase.firestore();
+var dbUsers = db.collection("users"); // Database of all stored game records
+var dbPlayers = db.collection("players"); // Database of two current player records
+var dbChat = db.collection("chatlog"); // Database of chatlog
 
-firebase.initializeApp(config);
+var wins = 0;
+var losses = 0;
+var draws = 0;
 
-var database = firebase.database();
+var userTitle = "Spectator";
+var userName = "";
+var userWeapon = "";
+var opponentWeapon = "";
 
+// FUNCTIONS
+function writeUser() {
+    dbUsers.doc(userName).set({
+        wins: wins,
+        draws: draws,
+        losses: losses
+    })
+}
+
+function writePlayer() {
+    dbPlayers.doc(userTitle).set({
+        userName: userName,
+        userWeapon: userWeapon
+    })
+}
+
+function battle() {
+    dbPlayers.get().then(function(snap) {
+        snap.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            // if () {
+
+            // }
+        })
+    })
+}
+
+// LISTENERS
+
+// changes to Player database to detect two filled-in userWeapon fields (which means a battle is possible)
+dbPlayers.onSnapshot(function() {
+    dbPlayers.where("userWeapon", ">", "").get().then(function(snap) {
+        if(snap.size == 2) {
+            battle();
+        }
+    })
+})
+
+
+
+// BUTTONS
+$("#connectButton").on("click", function(event) {
+    
+    if (userName == "") {
+        event.preventDefault();
+        userName = $("#nameInput").val().trim();
+
+        dbUsers.doc(userName).get().then(function(doc) {
+            if (doc.exists) {
+                wins = doc.data().wins;
+                losses = doc.data().losses;
+                draws = doc.data().draws;
+            } else {
+                writeUser();
+            }
+
+            // Only manual display update; after this, will be a listener event
+            $("#wins").html(wins);
+            $("#losses").html(losses);
+            $("#draws").html(draws);
+        }).catch(function(error) {
+            console.log("Error getting document: ", error)
+        })
+
+        dbPlayers.get().then(function(snap) {
+            if(snap.size < 2) {
+                userTitle = "Player " + userName;
+                writePlayer();
+                $("#display").text("CHOOSE WEAPON");
+                $("#hiddenRow1").removeClass("hidden");
+                $("#stats").removeClass("hidden");
+                $("#chat").removeClass("hidden");
+                $("#userStateTitle").text(userTitle);
+            } else {
+                userTitle = "Spectator";
+                $("#display").text("UNAVAILABLE");
+            }
+        })
+    }
+})
+
+$(".weapon").on("click", function() {
+    if (userWeapon === "" && userTitle != "Spectator") {
+        userWeapon = $(this).data("value");
+        $("#userWChoice").html("<i class='far fa-hand-" + userWeapon + "'></i>");
+        $("#hiddenRow2").removeClass("hidden");
+        writePlayer();
+    }
+})
