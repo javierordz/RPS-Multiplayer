a3d4b8fcdb665b02a3bc4b30618206d4adcae264
javierordz <javier.ordz@gmail.com>
2019-03-23 02:58:17 -0500

    readme.md

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // VARIABLES
 var db = firebase.firestore();
-var dbUsers = db.collection("users"); // Database of all stored game records
-var dbPlayers = db.collection("players"); // Database of two current player records
+var dbUsers = db.collection("users"); // Database of all stored Users' game records
+var dbPlayers = db.collection("players"); // Database of two CURRENT player choices
 var dbChat = db.collection("chatlog"); // Database of chatlog
 
 var wins = 0;
@@ -12,6 +12,7 @@ var userTitle = "Spectator";
 var userName = "";
 var userWeapon = "";
 var opponentWeapon = "";
+var playerArray= [];
 
 // FUNCTIONS
 function writeUser() {
@@ -19,39 +20,89 @@ function writeUser() {
         wins: wins,
         draws: draws,
         losses: losses
-    })
-}
+    });
+};
 
 function writePlayer() {
     dbPlayers.doc(userTitle).set({
         userName: userName,
         userWeapon: userWeapon
-    })
-}
+    });
+};
 
 function battle() {
-    dbPlayers.get().then(function(snap) {
-        snap.forEach(function(doc) {
-            console.log(doc.id, " => ", doc.data());
-            // if () {
+    dbPlayers.get().then(function(snapshot) {
+        snapshot.forEach(function(snap) {
+            playerArray.push(snap.data().userWeapon);
+        });
 
-            // }
-        })
+        playerArray.splice(playerArray.indexOf(userWeapon), 1);
+        opponentWeapon = playerArray.toString();
+        console.log(opponentWeapon);
+        $("#oppWChoice").html("<i class='far fa-hand-" + opponentWeapon + "'></i>");
+
+        if (userWeapon == opponentWeapon) {
+            draws++;
+            $("#display").text("...draw...");
+        } else if (userWeapon == "rock" && opponentWeapon == "scissors" ||
+                    userWeapon == "paper" && opponentWeapon == "rock" ||
+                    userWeapon == "scissors" && opponentWeapon == "paper" ) {
+            wins++;
+            $("#display").text("VICTORY!");
+        } else {
+            losses++;
+            $("#display").text("Deafeat.");
+        }
+
+        writeUser();
+        
+        dbPlayers.doc("Player " + userName).update({
+            userWeapon: firebase.firestore.FieldValue.delete()
+        }).then(function() {
+            setTimeout(function() { 
+                $("#userWChoice").html(""); 
+                $("#oppWChoice").html(""); 
+                $("#display").text("CHOOSE WEAPON"); 
+                userWeapon = "";
+                opponentWeapon = [];
+                playerArray = [];
+            }, 3000);
+        });
     })
 }
 
 // LISTENERS
-
 // changes to Player database to detect two filled-in userWeapon fields (which means a battle is possible)
 dbPlayers.onSnapshot(function() {
-    dbPlayers.where("userWeapon", ">", "").get().then(function(snap) {
-        if(snap.size == 2) {
-            battle();
+    dbPlayers.where("userWeapon", ">", "").get().then(function(snapshot) {
+        if (snapshot.size > 1) {
+            snapshot.forEach(function(snap) {
+                playerArray.push(snap.data().userWeapon);
+            });
+
+            playerArray.splice(playerArray.indexOf(userWeapon), 1);
+            opponentWeapon = playerArray.toString();
+
+            if (opponentWeapon != "") { 
+                $("#oppWChoice").html("<i class='far fa-hand-" + opponentWeapon + "'></i>"); 
+            }
+
+            opponentWeapon = [];
+            playerArray = [];
         }
-    })
+
+        if(snapshot.size == 2) { 
+            battle(); 
+        }
+    });
 })
 
-
+// changes to User record reflected in #statsCard
+dbUsers.onSnapshot(function(snap) {
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $("#draws").html(draws);
+})
 
 // BUTTONS
 $("#connectButton").on("click", function(event) {
@@ -92,7 +143,7 @@ $("#connectButton").on("click", function(event) {
             }
         })
     }
-})
+});
 
 $(".weapon").on("click", function() {
     if (userWeapon === "" && userTitle != "Spectator") {
@@ -100,5 +151,5 @@ $(".weapon").on("click", function() {
         $("#userWChoice").html("<i class='far fa-hand-" + userWeapon + "'></i>");
         $("#hiddenRow2").removeClass("hidden");
         writePlayer();
-    }
-})
+    };
+});
